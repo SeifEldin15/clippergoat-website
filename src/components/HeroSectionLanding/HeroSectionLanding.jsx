407275e3bd76513a2de130d4e82cd8a156f922e9
seifeldin15 <seifeldin02@outlook.com>
2024-08-19 01:39:13 +0300

    first commit

--- src/components/HeroSectionLanding/HeroSectionLanding.jsx
+++ src/components/HeroSectionLanding/HeroSectionLanding.jsx
@@ -2,8 +2,6 @@ import React, { useRef, useState, useEffect } from "react";
 import { motion, useMotionValue, useSpring, useTransform } from "framer-motion";
 import "./HeroSectionLanding.css";
 import ChallengeCard from "../ChallengeCard/ChallengeCard";
-import vid1 from "../../assets/videos/9;16 Hero.mp4";
-import vid2 from "../../assets/videos/Updated Final.mp4";
 
 function Card3D({ children }) {
   const x = useMotionValue(0);
@@ -98,15 +96,15 @@ function HeroSectionLandingHero() {
 
   const heroVideoSources = [
     {
-      src: vid1,
+      src: "https://clippergoat.nyc3.digitaloceanspaces.com/assets/9_16 hero-BSZAWict.mp4",
       media: "(max-width: 600px)"
     },
     {
-      src: vid2,
+      src: "https://clippergoat.nyc3.digitaloceanspaces.com/assets/Updated final-BDiza0Ii.mp4",
       media: "(min-width: 601px)"
     }
-  ]; 
-  
+  ];
+
   return (
     <>
 
