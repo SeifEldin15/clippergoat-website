59c07aacaaaf0c8cfbe9e96675075bbd7e771a12
SeifEldin15 <144861107+SeifEldin15@users.noreply.github.com>
2024-09-04 10:51:05 +0300

    Update Slider.jsx

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,34 +1,53 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './Slider.css';
 
+// Import images
+import ComedyReels from '../../assets/sliders/Comedy Reels-DEbXlLpl.png';
+import CookingYoutube from '../../assets/sliders/Cooking Youtube-BBUomc_9.png';
+import CryptoShorts from '../../assets/sliders/Crypto Shorts-D_nE0xtX.png';
+import DIYYoutube from '../../assets/sliders/DIY Youtube-K_qqiXen.png';
+import EcomAffiliate from '../../assets/sliders/Ecom Affiliate-DarCuoO9.png';
+import FacebookShort from '../../assets/sliders/facebook short-BfKl-8lo.png';
+import FinanceInvestmentReels from '../../assets/sliders/Finance _ Investment Reels-ZJPLBh0f.png';
+import GamblingAffiliate from '../../assets/sliders/Gambaling Affilate-X8QwuSo1.png';
+import FitnessAffiliate from '../../assets/sliders/Fitness Affiliate-Dfkp4T5U.png';
+import Influncer from '../../assets/sliders/Influncer -CuRdbXMl.png';
+import ProhibitedIndustries from '../../assets/sliders/Prohibited Industries-Dw9iO4Ar.png';
+import RealEstateShorts from '../../assets/sliders/Real estate shorts-DMnWXl6c.png';
+import StreamingShorts from '../../assets/sliders/Streaming Shorts-CgRvd7id.png';
+import TechReviewShorts from '../../assets/sliders/Tech Review Shorts-BjRzJc8Z.png';
+import TikTokAffiliate from '../../assets/sliders/TikTok Affiliate-Dka6IhO3.png';
+import TikTokShop from '../../assets/sliders/TikTok Shop-DzxUHX-e.png';
+import TravelTiktok from '../../assets/sliders/Traval Tiktok-fIqPFtPJ.png';
+import YoutubeInfluncer from '../../assets/sliders/Youtube Influncer-BCikUbZm.png';
 
 const Slider = () => {
   const [position, setPosition] = useState(0);
   const trackRef = useRef(null);
   const images = [
-    { src: "../../assets/sliders/Comedy Reels-DEbXlLpl.png", title: 'Felix', description: 'Comedy Reels' },
-    { src: "../../assets/sliders/Cooking Youtube-BBUomc_9.png", title: 'Jane', description: 'Cooking Youtuber' },
-    { src: "../../assets/sliders/Crypto Shorts-D_nE0xtX.png", title: 'Max', description: 'Crypto Shorts' },
-    { src: "../../assets/sliders/DIY Youtube-K_qqiXen.png", title: 'Emma', description: 'DIY Youtuber' },
-    { src: "../../assets/sliders/Ecom Affiliate-DarCuoO9.png", title: 'Liam', description: 'Ecom Affiliate' },
-    { src: "../../assets/sliders/facebook short-BfKl-8lo.png", title: 'Sophia', description: 'Facebook Short' },
-    { src: "../../assets/sliders/Finance _ Investment Reels-ZJPLBh0f.png", title: 'Lucas', description: 'Finance & Investment Reels' },
-    { src: "../../assets/sliders/Gambaling Affilate-X8QwuSo1.png", title: 'Noah', description: 'Gambling Affiliate' },
-    { src: "../../assets/sliders/Fitness Affiliate-Dfkp4T5U.png", title: 'Mia', description: 'Fitness Affiliate' },
-    { src: "../../assets/sliders/Gambaling Affilate-X8QwuSo1.png", title: 'Olivia', description: 'Gambling Affiliate' },
-    { src: "../../assets/sliders/Gambaling Affilate-X8QwuSo1.png", title: 'Ethan', description: 'Gaming Reels' },
-    { src: "../../assets/sliders/Influncer -CuRdbXMl.png", title: 'Ava', description: 'Influencer' },
-    { src: "../../assets/sliders/Ecom Affiliate-DarCuoO9.png", title: 'Alexander', description: 'Prank Youtuber' },
-    { src: "../../assets/sliders/Prohibited Industries-Dw9iO4Ar.png", title: 'Isabella', description: 'Prohibited Industries' },
-    { src: "../../assets/sliders/Real estate shorts-DMnWXl6c.png", title: 'James', description: 'Real Estate Shorts' },
-    { src: "../../assets/sliders/Streaming Shorts-CgRvd7id.png", title: 'Amelia', description: 'Streaming Shorts' },
-    { src: "../../assets/sliders/Tech Review Shorts-BjRzJc8Z.png", title: 'William', description: 'Tech Review Shorts' },
-    { src: "../../assets/sliders/TikTok Affiliate-Dka6IhO3.png", title: 'Charlotte', description: 'TikTok Affiliate' },
-    { src: "../../assets/sliders/TikTok Shop-DzxUHX-e.png", title: 'Benjamin', description: 'TikTok Shop' },
-    { src: "../../assets/sliders/Traval Tiktok-fIqPFtPJ.png", title: 'Elijah', description: 'Travel Tiktok' },
-    { src: "../../assets/sliders/Youtube Influncer-BCikUbZm.png", title: 'Harper', description: 'Youtube Influencer' }
+    { src: ComedyReels, title: 'Felix', description: 'Comedy Reels' },
+    { src: CookingYoutube, title: 'Jane', description: 'Cooking Youtuber' },
+    { src: CryptoShorts, title: 'Max', description: 'Crypto Shorts' },
+    { src: DIYYoutube, title: 'Emma', description: 'DIY Youtuber' },
+    { src: EcomAffiliate, title: 'Liam', description: 'Ecom Affiliate' },
+    { src: FacebookShort, title: 'Sophia', description: 'Facebook Short' },
+    { src: FinanceInvestmentReels, title: 'Lucas', description: 'Finance & Investment Reels' },
+    { src: GamblingAffiliate, title: 'Noah', description: 'Gambling Affiliate' },
+    { src: FitnessAffiliate, title: 'Mia', description: 'Fitness Affiliate' },
+    { src: GamblingAffiliate, title: 'Olivia', description: 'Gambling Affiliate' },
+    { src: GamblingAffiliate, title: 'Ethan', description: 'Gaming Reels' },
+    { src: Influncer, title: 'Ava', description: 'Influencer' },
+    { src: EcomAffiliate, title: 'Alexander', description: 'Prank Youtuber' },
+    { src: ProhibitedIndustries, title: 'Isabella', description: 'Prohibited Industries' },
+    { src: RealEstateShorts, title: 'James', description: 'Real Estate Shorts' },
+    { src: StreamingShorts, title: 'Amelia', description: 'Streaming Shorts' },
+    { src: TechReviewShorts, title: 'William', description: 'Tech Review Shorts' },
+    { src: TikTokAffiliate, title: 'Charlotte', description: 'TikTok Affiliate' },
+    { src: TikTokShop, title: 'Benjamin', description: 'TikTok Shop' },
+    { src: TravelTiktok, title: 'Elijah', description: 'Travel Tiktok' },
+    { src: YoutubeInfluncer, title: 'Harper', description: 'Youtube Influencer' }
   ];
-  
+
   
   const tripleImages = [...images, ...images, ...images];
 
